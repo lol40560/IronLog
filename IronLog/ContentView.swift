6c352e1685aaa4bf3ac05ae9e1c0f779f9728697
Quentin Chih <quentin20091222@gmail.com>
2026-09-07 12:53:00 +0800

    Initial Commit

---
+++ IronLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IronLog
+//
+//  Created by Quentin Chih on 2026/9/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IronLog/IronLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  IronLogApp.swift
+//  IronLog
+//
+//  Created by Quentin Chih on 2026/9/7.
+//
+
+import SwiftUI
+
+@main
+struct IronLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
